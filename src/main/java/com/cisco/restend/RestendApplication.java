107e7f312c721bcc917c20f8f5132ad2389b4437
sovisrushain <sovisrushain@gmail.com>
2022-03-16 10:44:22 +0530

    Initial Commit

---
+++ src/main/java/com/cisco/restend/RestendApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.restend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cisco/restend/RestendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.restend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
